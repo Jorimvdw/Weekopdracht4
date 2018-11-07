4a9f550b032ed1421cb28d23f1046a843597cf4a
Jorimvdw <jorimvanderwijngaard@gmail.com>
2018-11-07 14:41:54 +0100

    Monteurprijzen

--- src/Zwembad/Monteur.java
+++ src/Zwembad/Monteur.java
@@ -1,12 +1,13 @@
 package Zwembad;
 
 public class Monteur {
-
+	final static int reparatiePrijs = 500;
+	final static int onderhoudPrijs = 100;
+	
 	void onderhouden(Onderhoudbaar o) {
 		o.onderhouden(100);
 	}
 	void repareren (Onderhoudbaar o) {
 		o.repareren(false, 100);
-	}
-	
+	}	
 }
